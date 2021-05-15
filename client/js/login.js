c8b44ddc505b7a9f1146f4bd1dbb02090840d9e5
Casemiro16 <inaraimet@mail.ru>
2021-05-15 08:37:44 +0300

    Commit2.3

---
+++ client/js/login.js
@@ -0,0 +1,42 @@
+var main = function () {
+	"use strict";
+	$(".submit").on("click", function() {
+		var login = $('#login_window .form_request input[name="login"]').val();
+		var password = $('#login_window .form_request input[name="password"]').val();
+		if (login !== null && login.trim() !== "") {
+			console.log('email: ' + login + ' password: ' + password);
+			$.ajax({
+				url: '/accounts',
+				type: 'POST',
+				data: { 'email': login,
+						'password': password }
+			}).done(function(responde) {
+				window.location.replace('users/' + responde._id + '/');
+			}).fail(function(jqXHR, textStatus, error) {
+				console.log(error);
+				alert("Пользователь не найден.\nПроверьте верность введенных данных и повторите попытку");
+				$('#login_window .form_request input[name="password"]').val("");
+			});
+		}
+	});
+	console.log("login.js выполнен");
+}
+
+$(document).ready(main);
+
+/*
+<div id="login_window">
+	   	<div id="okno_login">
+	       	<h2 class="h2_request">
+	       		Введите данные для входа
+	       	</h2>
+	       	<div class="form_request">
+	       		<p>Вы можете войти в систему,<br>заполнив данную форму:</p>
+	       		<input type="login" name="login" placeholder="Логин">
+	       		<input type="password" name="password" placeholder="Пароль">
+	       	</div>
+	        <a href="#" class="close">Закрыть окно</a>
+	        <a href="#" class="submit">Войти</a> <!--Пока что тоже только закрывает-->
+	   	</div>
+	</div>
+*/
---
+++ client/js/register.js
@@ -0,0 +1,80 @@
+var main = function () {
+	"use strict";
+	$(".submit_register").on("click", function() {
+		var full_name = $('#register_window input[name="full_name"]').val(),
+			date = $('#register_window input[name="date"]').val(),
+			tel = $('#register_window input[name="tel"]').val(),
+			email = $('#register_window input[name="email"]').val(),
+			password = $('#register_window input[name="password"]').val(),
+			repeat_password = $('#register_window input[name="repeat_password"]').val();
+		if (repeat_password !== password) {
+			alert("Пароли не совпадают!\nВведите пароли снова и повторите попытку");
+			$('#register_window input[name="password"]').val("");
+			$('#register_window input[name="repeat_password"]').val("");
+		} else if ((full_name !== null && full_name.trim() !== "") && (date !== null && date.trim() !== "")) {
+			if ((tel !== null && tel.trim() !== "") && (email !== null && email.trim() !== "") && (password !== null && password.trim() !== "")) {
+				$.ajax({
+					url: '/users',
+					type: 'POST',
+					data: { 'username': full_name,
+							'date_of_birth': date,
+							'phone': tel,
+							'email': email,
+							'password': password }
+				}).done(function(responde) {
+					console.log(responde);
+					alert('Аккаунт успешно создан!');
+				}).fail(function(jqXHR, textStatus, error) {
+					console.log(error);
+					if (jqXHR.status === 501) {
+						alert("Такой пользователь уже существует!\nИзмените логин и повторите "
+						+ "попытку");
+					} else {					
+						alert("Произошла ошибка!\n"+jqXHR.status + " " + jqXHR.textStatus);	
+					}
+				});
+			}
+		}
+	});
+}
+
+$(document).ready(main);
+
+/*
+	<div id="register_window">
+	   	<div id="okno_register">
+	       	<h2 class="h2_register">
+	       		Введите данные для регистрации
+	       	</h2>
+	       	<div class="form_request">
+	       		<p>Вы можете зарегистрироваться в системе, заполнив данную форму:</p>
+	       		<div class="inputbox_register_left">
+	       			<label for="date">Введите свое полное имя:</label>
+	       			<input type="full_name" name="full_name" placeholder="Фамилия Имя Отчество">
+	       		</div>
+	       		<div class="inputbox_register_right">
+	       			<label for="date">Дата рождения:</label>
+	       			<input type="date" name="date" placeholder="Дата рождения">
+	       		</div>
+	       		<div class="inputbox_register_left">
+	       			<label for="tel">Номер телефона:</label>
+	       			<input type="tel" name="tel" placeholder="+7 (999) 999-99-99">
+	       		</div>
+	       		<div class="inputbox_register_right">
+	       			<label for="email">Электронная почта:</label>
+	       			<input type="email" name="email" placeholder="name@email.com">
+	       		</div>
+	       		<div class="inputbox_register_left">
+	      			<label for="password">Придумайте пароль:</label>
+	       			<input type="password" name="password" placeholder="*********">
+	       		</div>
+	       		<div class="inputbox_register_right">
+	       			<label for="password">Повторите пароль:</label>
+	       			<input type="password" name="repeat_password" placeholder="*********">
+	       		</div>     		
+        	</div>
+	        <a href="#" class="submit_register">Зарегистрироваться</a> <!--Пока что тоже только закрывает-->
+	        <a href="#" class="close_register">Закрыть окно</a>
+      	</div>
+    </div>
+   */
